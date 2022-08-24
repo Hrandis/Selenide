2941a77e0fca3fc3b7e966bcb445cc6554407d83
Hrandis <maks.ekaterina.alex@gmail.com>
2022-08-24 18:37:04 +0300

    Test improved

--- src/test/java/FormSendTest.java
+++ src/test/java/FormSendTest.java
@@ -1,9 +1,11 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -11,22 +13,27 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class FormSendTest {
 
-    LocalDate nowDate = LocalDate.now();
-    LocalDate nextWeekDate = nowDate.plusDays(7);
-    int year = nextWeekDate.getYear();
-    int month = nextWeekDate.getMonthValue();
-    int day = nextWeekDate.getDayOfMonth();
-    String dateForForm;
+//    LocalDate nowDate = LocalDate.now();
+//    LocalDate nextWeekDate = nowDate.plusDays(7);
+//    int year = nextWeekDate.getYear();
+//    int month = nextWeekDate.getMonthValue();
+//    int day = nextWeekDate.getDayOfMonth();
+//    String dateForForm;
+//
+//    @BeforeEach
+//    public void NextWeekDate() {
+//
+//        if (month < 10) {
+//            dateForForm = day + ".0" + month + "." + year;
+//        } else {
+//            dateForForm = day + "." + month + "." + year;
+//        }
+//    }
 
-    @BeforeEach
-    public void NextWeekDate() {
-
-        if (month < 10) {
-            dateForForm = day + ".0" + month + "." + year;
-        } else {
-            dateForForm = day + "." + month + "." + year;
-        }
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+    String dateForForm = generateDate(7);
 
     @Test
     void shouldSendFormWithCorrectData() {
@@ -39,6 +46,8 @@ public class FormSendTest {
         $("[data-test-id=\"phone\"] input").setValue("+79999999999");
         $("[data-test-id=\"agreement\"]").click();
         $(".button").click();
-        $("[data-test-id=\"notification\"]").shouldBe(visible, Duration.ofSeconds(15));
+        //$("[data-test-id=\"notification\"]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateForForm), Duration.ofSeconds(15))
+                                             .shouldBe(visible);
     }
 }
